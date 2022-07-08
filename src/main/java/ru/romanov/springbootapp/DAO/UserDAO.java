38fe21ea678b3e248d8ea5269ede0542ad5b45b4
RomanovVS <105357348+RomanovVS@users.noreply.github.com>
2022-07-08 13:57:46 +0300

    Add files via upload

---
+++ src/main/java/ru/romanov/springbootapp/DAO/UserDAO.java
@@ -0,0 +1,14 @@
+package ru.romanov.springbootapp.DAO;
+
+
+import ru.romanov.springbootapp.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    void saveUser(User user);
+    void deleteUser(Long id);
+    List<User> showAllUsers();
+    void editUser(Long id, User user);
+    User getUserById(Long id);
+}
